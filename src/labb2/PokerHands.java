f138298a022a8e9fcd80e52722fdb1957b28542e
moawahlgren <77838783+moawahlgren@users.noreply.github.com>
2021-02-02 13:40:11 +0100

    update
    hfih

--- src/labb2/PokerHands.java
+++ src/labb2/PokerHands.java
@@ -1,5 +1,14 @@
-package ps;
-import cardutils.*; 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psmodel;
+import cardutils.Pile;
+import cardutils.Suit;
+import cardutils.Rank; 
+import cardutils.Card; 
+import java.util.ArrayList;
 
 /**
  *
@@ -7,26 +16,27 @@ import cardutils.*;
  */
 public class PokerHands {
     
-    
-    private PokerHands() {
-    //Konstruktor 
-}
-    public PokerCombo getPokerCombo(Pile hand) {
+    public static PokerCombo getPokerCombo(Pile hand) {
         //Ska undersöka om det finns par, tvåpar, triss, färg, fyrtal 
         //Bläddra igenom alla rank och kolla om det finns två (par), tre(triss), fyra(fytal) av någon rank.
         //Behöver sedan kolla om det finns fler par (kort av samma rank) 
-        
-        
         //Behöver först kolla om färg, och isf returnera detta, annars kolla om par eller så
-        for(int i=0; i < 4; i++) {
-            int nrOfThatSuit = hand.nrOfSuit(Suit.values()[i]); 
+        
+        ArrayList handCopy = hand.getCards(); 
+        Pile pileCopy = new Pile(); 
+        pileCopy.add(handCopy);
+        
+        
+        
+        for(int i=0; i < Suit.values().length ; i++) {
+            int nrOfThatSuit = pileCopy.nrOfSuit(Suit.values()[i]); 
             if(nrOfThatSuit == 5) {
                 return PokerCombo.FLUSH;
             }
         }
         
-        for(int i=0; i < 13; i++) {
-            int nrOfThisRank = hand.nrOfRank(Rank.values()[i]);
+        for(int i=0; i < Rank.values().length; i++) {
+            int nrOfThisRank = pileCopy.nrOfRank(Rank.values()[i]);
             
             if(nrOfThisRank == 2) {
                 //Om det finns två av samma:
@@ -34,14 +44,14 @@ public class PokerHands {
                 //Removear dessa kort och letar sedan efter fler par genom att använda nrOfRank
                 //Hittar den då en trea, returnera triss istället, hittar den två, returnera två par
                 //Annars bara returnera par 
-                for(int j=0; j < hand.getSize(); j++) {
-                    Card c = hand.get(j); 
+                for(int j=0; j < pileCopy.getSize() ; j++) {
+                    Card c = pileCopy.get(j); 
                     if(c.getRank() == Rank.values()[i]) {
-                        hand.remove(c); 
+                        pileCopy.remove(c); 
                     }
                 }
-                for(int k=0; k < 13; k++) {
-                    int nrOfThatRank = hand.nrOfRank(Rank.values()[k]); 
+                for(int k=0; k < Rank.values().length; k++) {
+                    int nrOfThatRank = pileCopy.nrOfRank(Rank.values()[k]); 
                     
                     if(nrOfThatRank == 2) {
                         return PokerCombo.TWO_PAIRS;
